d796a139473ca167697a96c7340c10b6bac0b389
Philip Perry <philip.perry2625@gmail.com>
2026-05-13 00:11:22 -0500

    Waveform visualization and plotting script
    Added a script for visualizing microphone signal waveforms.

--- MATLAB/waveform_ plot.m
+++ MATLAB/waveform_ plot.m
@@ -1,1 +1,19 @@
+% Waveform Visualization Script
+% Smart Voice Classification System
 
+clear;
+clc;
+
+fs = 1000;                 % Sampling frequency
+t = 0:1/fs:1;              % Time vector
+
+% Example waveform
+signal = sin(2*pi*50*t);
+
+figure;
+plot(t, signal);
+
+xlabel('Time (s)');
+ylabel('Amplitude');
+title('Microphone Signal Waveform');
+grid on;
