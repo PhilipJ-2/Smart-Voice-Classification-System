13061eeec67d9d86b20407a19c4d9f67606afd5b
Philip Perry <philip.perry2625@gmail.com>
2026-05-13 00:11:00 -0500

    Bode plot and frequency response analysis
    Added Bode plot generation for an RC filter analysis.

--- MATLAB/bode_ analysis.m
+++ MATLAB/bode_ analysis.m
@@ -1,1 +1,22 @@
+% Bode Plot and Frequency Response Analysis
+% Smart Voice Classification System
 
+clear;
+clc;
+
+% Example RC filter values
+R = 1000;          % Resistance in ohms
+C = 0.1e-6;        % Capacitance in farads
+
+% Transfer function
+num = [1];
+den = [R*C 1];
+
+system_tf = tf(num, den);
+
+% Generate bode plot
+figure;
+bode(system_tf);
+grid on;
+
+title('RC Filter Bode Plot');
