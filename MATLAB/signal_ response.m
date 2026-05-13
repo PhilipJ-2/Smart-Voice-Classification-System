f0b645405462e9967bbec6c895212eb357b76342
Philip Perry <philip.perry2625@gmail.com>
2026-05-13 00:11:50 -0500

    Signal response and filtering analysis
    Added signal response analysis with low-pass filtering.

--- MATLAB/signal_ response.m
+++ MATLAB/signal_ response.m
@@ -1,1 +1,26 @@
+% Signal Response and Filtering Analysis
+% Smart Voice Classification System
 
+clear;
+clc;
+
+fs = 1000;
+t = 0:1/fs:1;
+
+% Example input signal
+input_signal = sin(2*pi*50*t) + 0.5*sin(2*pi*200*t);
+
+% Low-pass filter design
+fc = 100;
+[b, a] = butter(2, fc/(fs/2), 'low');
+
+% Filtered output
+filtered_signal = filter(b, a, input_signal);
+
+figure;
+plot(t, filtered_signal);
+
+xlabel('Time (s)');
+ylabel('Amplitude');
+title('Filtered Signal Response');
+grid on;
